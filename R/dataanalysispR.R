4788a4a5db697a1920aea8da47a5811556e4c754
Dusty Turner <dusty.s.turner@gmail.com>
2017-10-27 15:47:59 -0400

    testing

--- R/dataanalysispR.R
+++ R/dataanalysispR.R
@@ -1,109 +1,23 @@
-#' Puerto Rico Function
+#' this helper function
 #'
-#' This function takes a twitter dataframe and produces the twitter sentiment graph over time
-#' @param filename "Name of file to lookup -- csv"
-#' @keywords Puerto Rico
+#' helpers:
+#' @param filename "Puerto Rico 23 Sept - 26 OCT best.csv"
+#' @keywords Puerto Ricos
 #' @export
 #' @examples
-#' pRanalysis
+#' helpers()
+
+helpmes = function(filename = "best.csv"){
 
 
-pRanalysis = function(filename = "Puerto Rico 23 Sept - 26 OCT best.csv"){
-  
+  require(tidyr)
+  require(dplyr)
+  require(tidytext)
+  require(ggplot2)
+  require(lubridate)
 
-  library(tidyr)
-  library(dplyr)
-  library(tidytext)
-  library(ggplot2)
-  library(lubridate)
-  
-  df.tweet = read.csv(filename)
-  df.tweet$created = as.POSIXct(df.tweet$created,format = "%Y-%m-%d %H:%M", tz = "UTC")
-  # df.tweet$created = as.POSIXct(df.tweet$created,format = "%m/%d/%Y %H:%M", tz = "UTC")
-  df.tweet = df.tweet[,-c(1)]
-  max(df.tweet$created)
-  df.tweet = df.tweet[which(!is.na(df.tweet$text)),]
-  
-  
-  ######## tokenize
-  
-  text_df <- data_frame(line = 1:length(df.tweet$text), text = as.character(df.tweet$text), time = df.tweet$created)
-  
-  textcleaned = text_df %>%
-    unnest_tokens(word, text)
-  
-  
-  ##English Only
-  cleanedarticle = anti_join(textcleaned,stop_words, by = "word")
-  dropwords = data.frame(word = as.character(c("https","trump")))
-  cleanedarticle = anti_join(cleanedarticle,dropwords, by = "word")
-  
-  #### English
-  nrc = get_sentiments("nrc")
-  nrc = nrc %>%
-    filter(sentiment=="negative") %>%
-    full_join(nrc%>%
-                filter(sentiment=="positive"))
-  bing = get_sentiments("bing")
-  afinn = get_sentiments("afinn")
-  nrcspanish = read.csv("NRCSpanish.csv")
-  
-  max(cleanedarticle$time)
-  min(cleanedarticle$time)
-  
-  tweetsentimentbing <- cleanedarticle %>%
-    inner_join(bing, by = "word") 
-  tweetsentimentafinn <- cleanedarticle %>%
-    inner_join(afinn, by = "word") 
-  tweetsentimentnrc <- cleanedarticle %>%
-    inner_join(nrc, by = "word") 
-  tweetsentimentnrcspanish <- textcleaned %>%
-    inner_join(nrcspanish, by = c("word"="Spanish")) 
-  
-  tweetsentimentbing$sentimentnum = ifelse(tweetsentimentbing$sentiment=="positive",1,-1)
-  tweetsentimentnrc$sentimentnum = ifelse(tweetsentimentnrc$sentiment=="positive",1,-1)
-  tweetsentimentnrcspanish$sentimentnum = ifelse(tweetsentimentnrcspanish$Sentiment=="Positive",1,-1)
-  # tweetsentiment$sentimentnum = left_join(tweetsentiment)
-  
-  plottingsentimentbing = tweetsentimentbing %>%
-    mutate(windowday = as.POSIXct(trunc.POSIXt(time, units = c("hours")))) %>%
-    group_by(windowday) %>%
-    summarise(netsent = sum(sentimentnum), totalsent = sum(abs(sentimentnum))) %>%
-    mutate(sentavg = netsent/totalsent) %>%
-    mutate(Lexicon = "Bing")
-  
-  plottingsentimentafinn = tweetsentimentafinn %>%
-    mutate(windowday = as.POSIXct(trunc.POSIXt(time, units = c("hours")))) %>%
-    group_by(windowday) %>%
-    summarise(netsent = sum(score), totalsent = sum(abs(score))) %>%
-    mutate(sentavg = netsent/totalsent) %>%
-    mutate(Lexicon = "Afinn")
-  
-  plottingsentimentnrc = tweetsentimentnrc %>%
-    mutate(windowday = as.POSIXct(trunc.POSIXt(time, units = c("hours")))) %>%
-    group_by(windowday) %>%
-    summarise(netsent = sum(sentimentnum), totalsent = sum(abs(sentimentnum))) %>%
-    mutate(sentavg = netsent/totalsent) %>%
-    mutate(Lexicon = "NRC English")
-  
-  plottingsentimentnrcspanish = tweetsentimentnrcspanish %>%
-    mutate(windowday = as.POSIXct(trunc.POSIXt(time, units = c("hours")))) %>%
-    group_by(windowday) %>%
-    summarise(netsent = sum(sentimentnum), totalsent = sum(abs(sentimentnum))) %>%
-    mutate(sentavg = netsent/totalsent) %>%
-    mutate(Lexicon = "NRC Spanish")
-  
-  # plottingsentiment = bind_rows(plottingsentimentbing,plottingsentimentafinn,plottingsentimentnrc)
-  # plottingsentiment = bind_rows(plottingsentimentbing,plottingsentimentafinn,plottingsentimentnrc,plottingsentimentnrcspanish)
-  plottingsentiment = bind_rows(plottingsentimentnrc,plottingsentimentnrcspanish)
-  
-plot =  ggplot(data=plottingsentiment, aes(x=windowday, y = sentavg, color = Lexicon)) +
-    ylim(c(-1,1)) +
-    geom_smooth(span = .5) +
-    ggtitle("Twitter Sentiment Score Over Time", subtitle = paste("From", substr(min(plottingsentiment$windowday),1,10),"through",substr(max(plottingsentiment$windowday),1,10))) +
-    labs(caption=paste("Plot created:", Sys.Date())) +
-    labs(x="Date", y="Sentiment Score") 
-  
-  return(plot)
+  df = system.file("extdata", "best.csv", package = "puertoRicoR")
+  df = read.csv(df)
+  df = df$created
+  return(df)
 }
-
