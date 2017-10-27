cc70cc8ea551b43c0a75f179c0a7b87ac1d26a9a
Dusty Turner <dusty.s.turner@gmail.com>
2017-10-27 13:06:17 -0400

    asdf

--- R/puertoRicoR.R
+++ R/puertoRicoR.R
@@ -67,18 +67,32 @@ pRtwitter = function(searchterm = "Puerto Rico", n = 100000, since = '2017-10-25
     # df.tweet = distinct((full_join(df.tweet, motherofalldataframes, by = c("text", "created"))))
     df.tweet = df.tweet[which(!is.na(df.tweet$text)),]
     write.csv(df.tweet, newdataname)
-    return(paste("Your New Data Has Been Saved To", newdataname))
+    return(paste("Your New Data Has Been Added to your Old File", newdataname))
 
   } else if(file.exists(olddataname)==FALSE){
+    df = system.file("extdata", "best.csv", package = "puertoRicoR")
+    df.tweet = read.csv(df)
+    df.tweet$created = as.POSIXct(df.tweet$created,format = "%m/%d/%Y %H:%M", tz = "UTC")
+    df.tweet = df.tweet[,-c(1)]
+
+    #Adding newest data to old data in a csv
+    df.tweet$replyToSID = as.character(df.tweet$replyToSID)
+    df.tweet$id = as.character(df.tweet$id)
+    df.tweet$replyToUID = as.character(df.tweet$replyToUID)
+    df.tweet$text = iconv(df.tweet$text, from="UTF-8", to="ASCII", "byte")
+
 
     motherofalldataframes$longitude = as.numeric(motherofalldataframes$longitude)
     motherofalldataframes$latitude = as.numeric(motherofalldataframes$latitude)
     motherofalldataframes$created = as_datetime(motherofalldataframes$created)
     # motherofalldataframes$score = 0
     motherofalldataframes$text = iconv(motherofalldataframes$text, from="UTF-8", to="ASCII", "byte")
-    df.tweet = motherofalldataframes
+    df.tweet = rbind(df.tweet, motherofalldataframes)
+    df.tweet = distinct(df.tweet)
+    # df.tweet = distinct((full_join(df.tweet, motherofalldataframes, by = c("text", "created"))))
+    df.tweet = df.tweet[which(!is.na(df.tweet$text)),]
     write.csv(df.tweet, newdataname)
-    return(paste("Your New Data Has Been Saved To", newdataname))
+    return(paste("Your New Data Has Been Added to the basefile and saved as", newdataname))
 
   }
 
