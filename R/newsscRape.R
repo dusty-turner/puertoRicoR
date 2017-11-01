00bc6d69c91f90644392492f8a411f495a67b433
Dusty Turner <dusty.s.turner@gmail.com>
2017-11-01 15:47:08 -0400

    asdf

--- R/newsscRape.R
+++ R/newsscRape.R
@@ -21,9 +21,13 @@ google <- c("https://www.google.com/search?q=Puerto+Rico&num=100&tbs=sbd:1,qdr:m
             "https://www.google.com/search?q=Puerto+Rico&num=100&tbs=sbd:1,qdr:m&tbm=nws&ei=T3DwWZMux43CBJTEogg&start=400&sa=N&biw=1914&bih=947&dpr=1",
             "https://www.google.com/search?q=Puerto+Rico&num=100&tbs=sbd:1,qdr:m&tbm=nws&ei=YHDwWZMggv7ABNj7udAE&start=500&sa=N&biw=1914&bih=947&dpr=1",
             "https://www.google.com/search?q=Puerto+Rico&num=100&tbs=sbd:1,qdr:m&tbm=nws&ei=cXDwWc7AOML8wQTjnbDYBg&start=600&sa=N&biw=1914&bih=947&dpr=1",
-            "https://www.google.com/search?q=Puerto+Rico&num=100&tbs=sbd:1,qdr:m&tbm=nws&ei=hnDwWeeKCoP0wASPoImgCQ&start=700&sa=N&biw=1914&bih=947&dpr=1",
-            "https://www.google.com/search?q=Puerto+Rico&num=100&tbs=sbd:1,qdr:m&tbm=nws&ei=kHDwWcGnF8K3wATP1JvADg&start=800&sa=N&biw=1914&bih=947&dpr=1",
-            "https://www.google.com/search?q=Puerto+Rico&num=100&tbs=sbd:1,qdr:m&tbm=nws&ei=kHDwWcGnF8K3wATP1JvADg&start=800&sa=N&biw=1914&bih=947&dpr=1")
+            "https://www.google.com/search?q=Puerto+Rico&num=100&tbs=sbd:1,qdr:m&tbm=nws&ei=hnDwWeeKCoP0wASPoImgCQ&start=700&sa=N&biw=1914&bih=947&dpr=1")
+            # "https://www.google.com/search?q=Puerto+Rico&num=100&tbs=sbd:1,qdr:m&tbm=nws&ei=kHDwWcGnF8K3wATP1JvADg&start=800&sa=N&biw=1914&bih=947&dpr=1",
+            # "https://www.google.com/search?q=Puerto+Rico&num=100&tbs=sbd:1,qdr:m&tbm=nws&ei=kHDwWcGnF8K3wATP1JvADg&start=800&sa=N&biw=1914&bih=947&dpr=1")
+
+# google <- c("https://www.google.com/search?q=Puerto+Rico&num=100&tbs=sbd:1,qdr:m&tbm=nws&ei=ES71We7sHMre0gLFlZHYCQ&start=0&sa=N&biw=1366&bih=637&dpr=1",
+#             "https://www.google.com/search?q=Puerto+Rico&num=100&tbs=sbd:1,qdr:m&tbm=nws&ei=Fy71WfOBLYKJ0wLlkp2gCg&start=100&sa=N&biw=1366&bih=637&dpr=1",
+#             "https://www.google.com/search?q=Puerto+Rico&num=100&tbs=sbd:1,qdr:m&tbm=nws&ei=QS71WYKqKIiW0gLT_Y64Cg&start=200&sa=N&biw=1366&bih=637&dpr=1")
 
 
 ##headlines
@@ -31,15 +35,17 @@ headlinelist = list()
 blurblist = list()
 originslist = list()
 
-for(i in 1:length(google)){
+for(i in seq_along(google)){
   headlines <- google[i] %>%
     read_html() %>%
     html_nodes(".r") %>%
     html_text()
+  Sys.sleep(sample(10, 1) * 0.1)
   blurb <- google[i] %>%
     read_html() %>%
     html_nodes(".st") %>%
     html_text()
+  Sys.sleep(sample(10, 1) * 0.1)
   origins <- google[i] %>%
     read_html() %>%
     html_nodes(".slp , ._gJs") %>%
@@ -47,6 +53,7 @@ for(i in 1:length(google)){
   headlinelist[[i]] = headlines
   blurblist[[i]] = blurb
   originslist[[i]] = origins
+  Sys.sleep(sample(10, 1) * 0.1)
 }
 headlines = unlist(headlinelist)
 blurb = unlist(blurblist)
@@ -69,7 +76,7 @@ number = ifelse(stri_detect_fixed(Date1, "minute"),as.numeric(number1)*60,
                        ifelse(stri_detect_fixed(Date1, "day"),as.numeric(number1)*24*60*60,number1)))
 
 
-Date = trunc.POSIXt(vec - as.numeric(number), "days")
+Date = trunc.POSIXt(vec[1:length(number)] - as.numeric(number), "days")
 Date = as.POSIXct(Date)
 
 Date[is.na(Date)] = as.Date(Date1[is.na(Date)], "%b %d, %Y") + days(1)
@@ -77,7 +84,7 @@ Date = trunc.POSIXt(Date, "days")
 Date = as.POSIXct(Date)
 googleheadlines = data_frame(Source = Source, Date = Date, Title = headlines, Narrative = blurb)
 
-
+tail(dateloc)
 
 news = read.csv(oldfile)
 
