85276f09ef4c3a6e283261682cdee27c94ffcbde
Dusty Turner <dusty.s.turner@gmail.com>
2017-11-09 20:57:17 -0500

    update and make better

--- R/pRanalysis.R
+++ R/pRanalysis.R
@@ -7,7 +7,7 @@
 #' @examples
 #' pRtwitter()
 
-pRanalysis = function(filename = "Puerto Rico 23SEP - 1NOV.csv"){
+pRanalysis = function(filename = "Puerto Rico 23S - 9NOV.csv"){
 
 
   require(tidyr)
@@ -16,8 +16,9 @@ pRanalysis = function(filename = "Puerto Rico 23SEP - 1NOV.csv"){
   require(ggplot2)
   require(lubridate)
   require(zoo)
+  require(readr)
   # filename = "testingagainR.csv"
-  df.tweet = read.csv(filename)
+  df.tweet = read_csv(filename)
   df.tweet$created = as.POSIXct(df.tweet$created,format = "%Y-%m-%d %H:%M", tz = "UTC")
   # df.tweet$created = as.POSIXct(df.tweet$created,format = "%m/%d/%Y %H:%M", tz = "UTC")
   df.tweet = df.tweet[,-c(1)]
@@ -44,8 +45,8 @@ pRanalysis = function(filename = "Puerto Rico 23SEP - 1NOV.csv"){
     filter(sentiment=="negative") %>%
     full_join(nrc%>%
                 filter(sentiment=="positive"))
-  bing = get_sentiments("bing")
-  afinn = get_sentiments("afinn")
+  # bing = get_sentiments("bing")
+  # afinn = get_sentiments("afinn")
 
   df = system.file("extdata", "NRCSpanish.csv", package = "puertoRicoR")
   nrcspanish = read.csv(df)
@@ -53,35 +54,35 @@ pRanalysis = function(filename = "Puerto Rico 23SEP - 1NOV.csv"){
   max(cleanedarticle$time)
   min(cleanedarticle$time)
 
-  tweetsentimentbing <- cleanedarticle %>%
-    inner_join(bing, by = "word")
-  tweetsentimentafinn <- cleanedarticle %>%
-    inner_join(afinn, by = "word")
+  # tweetsentimentbing <- cleanedarticle %>%
+  #   inner_join(bing, by = "word")
+  # tweetsentimentafinn <- cleanedarticle %>%
+  #   inner_join(afinn, by = "word")
   tweetsentimentnrc <- cleanedarticle %>%
     inner_join(nrc, by = "word")
   tweetsentimentnrcspanish <- textcleaned %>%
     inner_join(nrcspanish, by = c("word"="Spanish"))
 
-  tweetsentimentbing$sentimentnum = ifelse(tweetsentimentbing$sentiment=="positive",1,-1)
+  # tweetsentimentbing$sentimentnum = ifelse(tweetsentimentbing$sentiment=="positive",1,-1)
   tweetsentimentnrc$sentimentnum = ifelse(tweetsentimentnrc$sentiment=="positive",1,-1)
   tweetsentimentnrcspanish$sentimentnum = ifelse(tweetsentimentnrcspanish$Sentiment=="Positive",1,-1)
   # tweetsentiment$sentimentnum = left_join(tweetsentiment)
 
-  plottingsentimentbing = tweetsentimentbing %>%
-    mutate(windowday = as.POSIXct(trunc.POSIXt(time, units = c("hours")))) %>%
-    group_by(windowday) %>%
-    summarise(netsent = sum(sentimentnum), totalsent = sum(abs(sentimentnum))) %>%
-    mutate(sentavg = netsent/totalsent) %>%
-    mutate(Lexicon = "Bing") %>%
-    mutate(rollingmean = rollmean(x = sentavg, 24, align = "right", fill = NA))
-
-  plottingsentimentafinn = tweetsentimentafinn %>%
-    mutate(windowday = as.POSIXct(trunc.POSIXt(time, units = c("hours")))) %>%
-    group_by(windowday) %>%
-    summarise(netsent = sum(score), totalsent = sum(abs(score))) %>%
-    mutate(sentavg = netsent/totalsent) %>%
-    mutate(Lexicon = "Afinn") %>%
-    mutate(rollingmean = rollmean(x = sentavg, 24, align = "right", fill = NA))
+  # plottingsentimentbing = tweetsentimentbing %>%
+  #   mutate(windowday = as.POSIXct(trunc.POSIXt(time, units = c("hours")))) %>%
+  #   group_by(windowday) %>%
+  #   summarise(netsent = sum(sentimentnum), totalsent = sum(abs(sentimentnum))) %>%
+  #   mutate(sentavg = netsent/totalsent) %>%
+  #   mutate(Lexicon = "Bing") %>%
+  #   mutate(rollingmean = rollmean(x = sentavg, 24, align = "right", fill = NA))
+  #
+  # plottingsentimentafinn = tweetsentimentafinn %>%
+  #   mutate(windowday = as.POSIXct(trunc.POSIXt(time, units = c("hours")))) %>%
+  #   group_by(windowday) %>%
+  #   summarise(netsent = sum(score), totalsent = sum(abs(score))) %>%
+  #   mutate(sentavg = netsent/totalsent) %>%
+  #   mutate(Lexicon = "Afinn") %>%
+  #   mutate(rollingmean = rollmean(x = sentavg, 24, align = "right", fill = NA))
 
   plottingsentimentnrc = tweetsentimentnrc %>%
     mutate(windowday = as.POSIXct(trunc.POSIXt(time, units = c("hours")))) %>%
@@ -105,7 +106,7 @@ pRanalysis = function(filename = "Puerto Rico 23SEP - 1NOV.csv"){
 
   plot =  ggplot(data=plottingsentiment, aes(x=windowday, y = rollingmean, color = Lexicon)) +
     ylim(c(-1,1)) +
-    geom_smooth(span = .10, aes(y = sentavg)) +
+    geom_smooth(method = "loess", span = .10, aes(y = sentavg)) +
     # geom_line() +
     ggtitle("Twitter Sentiment Score Over Time", subtitle = paste("From", substr(min(plottingsentiment$windowday),1,10),"through",substr(max(plottingsentiment$windowday),1,10))) +
     labs(caption=paste("Plot created:", Sys.Date())) +
