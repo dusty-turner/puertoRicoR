75f2b8a6b95483da49096373233a5ba58494b925
Dusty Turner <dusty.s.turner@gmail.com>
2018-03-02 20:01:53 -0500

    slashes for dates

--- R/pRanalysis.R
+++ R/pRanalysis.R
@@ -19,8 +19,8 @@ pRanalysis = function(filename = "Puerto Rico 23 Sept - 2 MAR best.csv"){
   require(scales)
   # filename = "testingagainR.csv"
   df.tweet = read_csv(filename)
-  df.tweet$created = as.POSIXct(df.tweet$created,format = "%Y-%m-%d %H:%M", tz = "UTC")
-  # df.tweet$created = as.POSIXct(df.tweet$created,format = "%m/%d/%Y %H:%M", tz = "UTC")
+  # df.tweet$created = as.POSIXct(df.tweet$created,format = "%Y-%m-%d %H:%M", tz = "UTC")
+  df.tweet$created = as.POSIXct(df.tweet$created,format = "%m/%d/%Y %H:%M", tz = "UTC")
   df.tweet = df.tweet[,-c(1)]
   max(df.tweet$created)
   df.tweet = df.tweet[which(!is.na(df.tweet$text)),]
